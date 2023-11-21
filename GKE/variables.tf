7bdae672d24f2c52c0ce4479faa10098e234dd5a
unknown <sadmanhoque@yahoo.com>
2023-11-21 18:02:58 -0400

    working on building the Iac for the GKE cluster now

---
+++ GKE/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "project name"
+  type        = string
+  default     = "test"
+}
+
+variable "region" {
+  description = "project region"
+  type        = string
+  default     = "us-central1"
+}
