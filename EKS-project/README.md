7930a034fa4327b48605f014ce5887e16f820bba
unknown <sadmanhoque@yahoo.com>
2023-12-05 22:35:03 -0400

    overhauled the entire script and the readme file is more fleshed out with all the commands but not enough explanations, the cluster is not properly working yet though

--- EKS-project/README.md
+++ EKS-project/README.md
@@ -9,6 +9,36 @@ kubernetes-manifest file documentation:
 
 https://kubernetes.io/docs/concepts/workloads/controllers/deployment/#progress-deadline-seconds
 
+Configure locally installed kubectl to connect with the deployed cluster
+
+`aws eks --region ca-central-1 update-kubeconfig --name my-eks-cluster`
+
+If kubectl is already configured for other clusters run:
+
+`kubectl config use-context <eks-cluster-name>`
+
+The precise cluster name needs to be retrieved from the list of clusters on on kubectl configuration, to get that run:
+
+`kubectl config get-contexts`
+
+To authenticate local kubectl with AWS so it can actually deployer worker nodes:
+
+`aws eks update-kubeconfig --region ca-central-1 --name my-eks-cluster`
+For more documentation on the process https://docs.aws.amazon.com/eks/latest/userguide/create-kubeconfig.html
+
+
+To deploy the worker node now run
+
+`kubectl apply -f test-k8s-manifest.yaml`
+
+To setup the ingress for the worker node
+
+`kubectl apply -f k8s-ingress-manifest.yaml`
+
+Now wait for the changes to be executed, use the console to figure out when it's done.
+
+After that run
+
 ## list of commands ##
 
 Initializing the project config
--- EKS-project/main.tf
+++ EKS-project/main.tf
@@ -1,96 +1,93 @@
-resource "aws_eks_cluster" "example" {
-  name     = "example"
-  role_arn = aws_iam_role.example.arn
+# Create VPC
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+}
+
+# Create subnets
+resource "aws_subnet" "eks_subnet_1" {
+  vpc_id     = aws_vpc.eks_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ca-central-1a"
+}
+
+resource "aws_subnet" "eks_subnet_2" {
+  vpc_id     = aws_vpc.eks_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ca-central-1b"
+}
+
+# Create EKS cluster
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.main.id, aws_subnet.second.id]
+    subnet_ids = [aws_subnet.eks_subnet_1.id, aws_subnet.eks_subnet_2.id]
   }
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
-  depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
-    aws_subnet.main,
-    aws_subnet.second,
-  ]
+  depends_on = [aws_subnet.eks_subnet_1, aws_subnet.eks_subnet_2]
 }
 
-output "endpoint" {
-  value = aws_eks_cluster.example.endpoint
+# Create IAM role for EKS cluster
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
 
-output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.example.certificate_authority[0].data
+# Attach policies to EKS cluster role
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
 }
 
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
+resource "aws_iam_role_policy_attachment" "eks_service_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_cluster.name
 }
 
-data "aws_availability_zones" "available" {
-  state = "available"
-}
+# Create Fargate Profile
+resource "aws_eks_fargate_profile" "fargate_profile" {
+  cluster_name = aws_eks_cluster.eks_cluster.name
+  fargate_profile_name = "default"
+  pod_execution_role_arn = aws_iam_role.example.arn
 
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-   availability_zone = data.aws_availability_zones.available.names[0]
+  subnet_ids = [aws_subnet.eks_subnet_1.id, aws_subnet.eks_subnet_2.id]
 
-  tags = {
-    Name = "Main"
-  }
-
-  depends_on = [
-    aws_vpc.main
-  ]
-}
-
-resource "aws_subnet" "second" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-   availability_zone = data.aws_availability_zones.available.names[1]
-
-  tags = {
-    Name = "Second"
-  }
-
-  depends_on = [
-    aws_vpc.main
-  ]
-}
-
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "main"
+  selector {
+    namespace = "default"
   }
 }
 
 resource "aws_iam_role" "example" {
-  name               = "eks-cluster-example"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  name = "eks-fargate-profile-example"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks-fargate-pods.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
 }
 
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.example.name
-}
-
-# Optionally, enable Security Groups for Pods
-# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceController" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSFargatePodExecutionRolePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
   role       = aws_iam_role.example.name
 }
